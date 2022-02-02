81dd4c9b43de4f19dafa39e13ab6e92029fa847a
Arun Singh <arsing@adobe.com>
2022-02-02 17:38:50 +0530

    For Demo

--- app.py
+++ app.py
@@ -40,13 +40,20 @@ def home_signin_json():
     return data
 
 
+def deposit_signin_read_json():
+    with open("Deposit Checks02.json") as f:
+        data = json.load(f)
+    return data
+
+
 required_json = read_json()
 payment_json = payment_read_json()
 deposit_json = deposit_read_json()
 money_json = moneytransfer_read_json()
 signin_json = home_signin_json()
-json_data = {"home": required_json, "Digital_Payments": deposit_json,
-             "Protect_Payments": payment_json, "Money_Transfer": money_json, "home_login": signin_json}
+deposit_signin_json = deposit_signin_read_json()
+json_data = {"home": required_json, "Digital_Payments": deposit_json, "Protect_Payments": payment_json,
+             "Money_Transfer": money_json, "home_login": signin_json, "Digital_Payments_sigin": deposit_signin_json}
 
 
 def get_nextstep(target, data):
@@ -59,7 +66,7 @@ def get_nextstep(target, data):
     return req_list, source_val
 
 
-def set_message(response, path, data):
+def set_message(response, path, data, current_loc, login_status):
     if data['type'] == "message":
         response["message"] = data["msg"]
         response["payload"]["type"] = data['type']
@@ -80,7 +87,15 @@ def set_message(response, path, data):
         response["payload"]["type"] = data['type']
         response["payload"]["message"] = data['msg']
         if data["msg"] in json_data.keys():
-            response = get_nextFlow(json_data[data["msg"]], data['msg'])
+            if current_loc == "Digital_Payments_sigin" and login_status == 'true':
+                response = get_nextFlow(
+                    json_data["Digital_Payments"], "Digital_Payments", login_status)
+            elif login_status == 'true' and data["msg"] == "Digital_Payments":
+                response = get_nextFlow(
+                    json_data["Digital_Payments_sigin"], "Digital_Payments_sigin", login_status)
+            else:
+                response = get_nextFlow(
+                    json_data[data["msg"]], data['msg'], login_status)
     elif data['type'] == "input":
         response["payload"]["type"] = data['type']
         response["targetid"] = path[0]['target']
@@ -169,14 +184,15 @@ def chatbot():
         "value": [], "message": ""}, "topic": ""}
     print(data)
     if data and data["source"]:
-
+        current_loc, login_status = data['topic'], data["login"]
         topic_json = json_data[data['topic']]
         path, source = get_nextstep(data['source'], topic_json)
         if path:
             box_msg = topic_json["page1"][0][source]
         else:
             box_msg = topic_json["page1"][0][data['source']]
-        response = set_message(response, path, box_msg)
+        response = set_message(response, path, box_msg,
+                               current_loc, login_status)
         while True:
             if response["payload"]["type"] in ["end", "buttons"]:
                 break
@@ -185,7 +201,8 @@ def chatbot():
                 box_msg = topic_json["page1"][0][source]
             else:
                 box_msg = topic_json["page1"][0][response["targetid"]]
-            response = set_message(response, path, box_msg)
+            response = set_message(
+                response, path, box_msg, current_loc, login_status)
         if not response["topic"]:
             response["topic"] = data['topic']
     else:
@@ -194,17 +211,19 @@ def chatbot():
             location = "home_login"
         else:
             location = "home"
+        current_loc, login_status = location, data["login"]
         path = [json_data[location]['page1'][1]
                 [json_data[location]['page1'][2]]]
         box_msg = json_data[location]["page1"][0][path[0]["source"]]
-        response = set_message(response, path, box_msg)
+        response = set_message(response, path, box_msg,
+                               current_loc, login_status)
         while True:
             if response["payload"]["type"] in ["end", "buttons"]:
                 break
             path, source = get_nextstep(
                 response["targetid"], json_data[location])
             response = set_message(
-                response, path, json_data[location]["page1"][0][source])
+                response, path, json_data[location]["page1"][0][source], current_loc, login_status)
         response["topic"] = location
         if data['login'] == 'false':
             response["payload"] = {}
@@ -214,19 +233,19 @@ def chatbot():
     return jsonify({"response": response})
 
 
-def get_nextFlow(req_json_data, topic):
+def get_nextFlow(req_json_data, topic, status):
     response = {"message": "", "payload": {"value": [], "message": ""}}
     path = [req_json_data['page1'][1][req_json_data['page1'][2]]]
     box_msg = req_json_data["page1"][0][path[0]["source"]]
 
-    response = set_message(response, path, box_msg)
+    response = set_message(response, path, box_msg, topic, status)
     while True:
 
         if response["payload"]["type"] in ["end", "buttons"]:
             break
         path, source = get_nextstep(response["targetid"], req_json_data)
         response = set_message(
-            response, path, req_json_data["page1"][0][source])
+            response, path, req_json_data["page1"][0][source], topic, status)
     response["topic"] = topic
     return response
 
